3bd472a3da831a6b309a125fcfa2cea6e173204c
张晓华 <wantfulai@163.com>
2025-04-29 22:19:38 +0800

    MyBatisPlus插件自动配置

--- hubbo-web-parent/hubbo-web-demo/src/main/java/cn/hubbo/WebApplication.java
+++ hubbo-web-parent/hubbo-web-demo/src/main/java/cn/hubbo/WebApplication.java
@@ -2,14 +2,14 @@
  * @formatter:off
  * Copyright (c) 张晓华
  * @author 张晓华
- * 创建时间: 2025.4.27.10.53
- * 更新时间: 2025/4/27 22:53
- * 限定类名: cn.hubbo.web.WebApplication
+ * 创建时间: 2025.4.29.10.19
+ * 更新时间: 2025/4/28 23:08
+ * 限定类名: cn.hubbo.WebApplication
  * 适度编码益脑，沉迷编码伤身，合理安排时间，享受快乐生活。
  * @formatter:on
  */
 
-package cn.hubbo.web;
+package cn.hubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
